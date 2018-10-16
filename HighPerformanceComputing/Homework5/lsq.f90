4aec50c66a3752294fc36d8d22c93de18dca9a24
fjcasti1 <fjcasti1@asu.edu>
2018-10-15 20:44:49 -0700

    HPC homework 5 started

--- HighPerformanceComputing/Homework5/lsq.f90
+++ HighPerformanceComputing/Homework5/lsq.f90
@@ -1,4 +1,4 @@
-   module lsq
+module lsq
 !  LSQ - Interface module for LAPACK QR routines, and a special-purpose
 !  subroutine for simple Y versus X linear regressions.
 !
@@ -18,47 +18,56 @@
 !    10/15/18 - first implementation.
 !
 !  PROGRAMMER
-!    Your name, email@asu.edu
+!    Francisco Castillo-Carrasco, fjcasti1@asu.edu
 !
-   use precision
-   implicit none
+  use precision
+  implicit none
 !
 !  Generic interface (for single- or double-precision) for LAPACK
 !  QR factorizations SGELS and DGELS.
 !
-   interface xgels
-      subroutine sgels(tr,n,k,nrhs,x,ldx,y,ldy,work,lwork,info)
-         import
-         character, intent(in):: tr
-         integer, intent(in):: n, k, ldx, ldy, nrhs
-         integer, intent(inout):: lwork
-         real(SP), intent(inout):: x(ldx,k), y(ldy,nrhs), work(lwork)
-         integer, intent(out):: info
-      end subroutine sgels
+  interface xgels
+    subroutine sgels(tr,n,k,nrhs,x,ldx,y,ldy,work,lwork,info)
+        import
+        character, intent(in):: tr
+        integer, intent(in):: n, k, ldx, ldy, nrhs
+        integer, intent(inout):: lwork
+        real(SP), intent(inout):: x(ldx,k), y(ldy,nrhs), work(lwork)
+        integer, intent(out):: info
+     end subroutine sgels
 !
-      subroutine dgels(tr, n, k, nrhs, x, ldx, y, ldy, work, lwork, info)
-         import
-         character, intent(in):: tr
-         integer, intent(in):: n, k, ldx, ldy, nrhs
-         integer, intent(inout):: lwork
-         real(DP), intent(inout):: x(ldx,k), y(ldy,nrhs), work(lwork)
-         integer, intent(out):: info
-      end subroutine dgels
-   end interface
+     subroutine dgels(tr, n, k, nrhs, x, ldx, y, ldy, work, lwork, info)
+        import
+        character, intent(in):: tr
+        integer, intent(in):: n, k, ldx, ldy, nrhs
+        integer, intent(inout):: lwork
+        real(DP), intent(inout):: x(ldx,k), y(ldy,nrhs), work(lwork)
+        integer, intent(out):: info
+     end subroutine dgels
+  end interface
 !
-   contains
+  contains
 !----------
-   subroutine linfit(model, n, x, y, param, ierr)
+  subroutine linfit(model, n, x, y, param, ierr)
 !  LINFIT - briefly describe the input and output arguments here.
 !
-   character(*), intent(in):: model
-   integer, intent(in):: n
-   real(DP), intent(in):: x(n), y(n)
-   real(DP), intent(out):: param(2)
-   integer, intent(out):: ierr
+  character(*), intent(in) :: model
+  integer , intent(in)  :: n
+  real(DP), intent(in)  :: x(n), y(n)
+  real(DP), intent(out) :: param(2)
+  integer , intent(out) :: ierr
+  real(DP) :: X(n,2)
+  X(:,1)=1
+  X(:,2)=x
+
 !
-!  your code here
+  select case(model(1:1))
+  case('L','l')
+    call dgels('N', n, 2, 1, X, n, y, n, work, -1, info)
+  case('P','p')
+  case default
+  end select
 !
-   end subroutine linfit
+  end subroutine linfit
 !-----------------------
-   end module lsq
+end module lsq
---
+++ HighPerformanceComputing/Homework5/test_linfit.f90
@@ -0,0 +1,17 @@
+program test_linfit
+  use linfit
+  use precision
+  implicit none
+  real(DP):: x(20), y(20), noise(20), param(2)
+  integer:: j
+
+  call random_number(noise) uniformly distributed in [0, 1]
+  do j=1,20
+  x(j) = j
+  y(j) = 1.0 + 2*x(j) + 0.05*(noise(j) - 0.5)
+  enddo
+  
+  call linfit('Linear',20,x,y,param,ierr)
+  if(ierr == 0) write(6,*) param
+
+end program test_linfit
