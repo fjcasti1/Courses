8e42ce680e273334728fbce17486e5f15ed3ebb6
fjcasti1 <fjcasti1@asu.edu>
2018-10-29 01:23:55 -0700

    HPC, Hw5, included documentation, changed a few names.

--- HighPerformanceComputing/Homework5/henondim.f90
+++ HighPerformanceComputing/Homework5/henondim.f90
@@ -6,21 +6,38 @@ module henondim
 !    use henondim
 !
 !  DESCRIPTION
-!    Write a paragraph that summarizes your computational approach to implement
-!    Algorithm D.
+!    The program testing this algorithm (test_henon) will pass a 1D array of
+!    epsilons, the dimension of this array, and the dimension d. The basin is
+!    calculated using the henon_map subroutine. Then the code, when running in
+!    parallel, will thread assigning a different epsilon to each thread. Next,
+!    for each epsilon, the subroutine henon_compare will compute basinP and
+!    basinM (the basins obtained of the grid shifted by + or - epsilon) and
+!    count the number of epsilon-uncertain grid points. To finish, it will use
+!    the subroutine linfit of the module lsq, to obtain the dimension (if the
+!    no erro flah is returned), which will be then passed to the testing
+!    program test_henon.
 !
 !  PUBLIC ROUTINES DEFINED
-!    Brief list of procedures that can be called from outside this module.
+!   - henon_map
+!   - basin_alg
+!   - basin_compare
 !
 !  REQUIRED DEPENDENCIES
 !    precision - to define KINDs for single and double floating-point.
-!    And any other modules that you define.
+!          lsq - to do the power fitting needed at the end of th Algorithm D. 
 !
 !  REVISION HISTORY
-!   10/15/18 - First implementation.
+!   10/19/18 - Code works using constructed Matlab functions linspace and
+!              meshgrid. Also the subroutine reshape since 2D arrays basins
+!              are used.
+!   10/24/18 - Removed all Matlab functions and reshaping. Adapted code to work
+!              with 1D array basins of dimension NGRID^2. Included subroutine
+!              basin_compare in order to use OpenMP. The subroutine will make
+!              sure each thread has individual copies of the basins to work with
+!              them.
 !
 !  PROGRAMMER
-!   Francisco Castillo, fjcasti1@asu.edu
+!   Francisco Castillo-Carrasco, fjcasti1@asu.edu
 !
   use lsq
   use precision
@@ -37,20 +54,25 @@ module henondim
 !  LOCKOUT units away from the origin.  (It suffices to check whether the
 !  |X_n| > LOCKOUT on the nth iteration.)
 !
-!  Argument declarations go here.  Explicitly declare all arguments and give
-!  each of them an INTENT.
+! ARGUMENTS - basin : logical, 1D array of dimension (NGRID*NGRID), True will
+!                     mean that the grid point tends to infinity.
+!           - eps   : real, eps represents the magnitude of the shifting
+!                     of the initial grid.
 !
-!
-!  Declare local variables here.  You may declare the map parameters A and B
-!  as Fortran PARAMETERS or simple local variables at your option.
-!
+! PROCEDURE : We define the initial grid using array constructors and do loops
+!             and store it in the 2D array X0. This array will evolve according
+!             to the equations giveni in the Henon map. The grid points will
+!             be filtered out using the commands where and all.
+
     implicit none
+    logical , intent(inout) :: basin(NGRID*NGRID)
+    real(DP), intent(in)    :: eps
+
     integer  :: j
+    real(DP) :: a = 2.12d0, b = -0.3d0, h
     real(DP), dimension (:)  , allocatable :: x, y
+    real(DP), dimension (:)  , allocatable :: aux 
     real(DP), dimension (:,:), allocatable :: X0 
-    real(DP), dimension (:), allocatable :: aux 
-    real(DP) :: a = 2.12d0, b = -0.3d0, eps, h
-    logical  :: basin(NGRID*NGRID)
 
     allocate(x(NGRID))
     allocate(y(NGRID))
@@ -79,10 +101,26 @@ module henondim
 
   end subroutine henon_map
 !-------------------------------------------------------------------------------
-  subroutine henon_compare(N,eps,basin)
+  subroutine basin_compare(N,eps,basin)
+! BASIN_COMPARE - counts the number of epsilon uncertain grid points.
+!
+! ARGUMENTS - N     : real, N represents the number of epsilon-uncertain points
+!                     for the given value of epsilon, eps.
+!           - eps   : real, eps represents the magnitude of the shifting of the
+!                     initial grid.
+!           - basin : logical, array of dimension NGRID^2, it is the original
+!                     unshifted basin, used to compare with the shifted ones and obtain
+!                     the number of epsilon-uncertain grid points.
+!
+! PROCEDURE : We simply use the henon_map subroutine to calculate the shifted
+!             basins and the function count to obtain the total number of grid
+!             points whose basins don't match. This is, the total number of
+!             epsilon-uncertain grid points.
+
+    implicit none
     real(DP), intent(in)    :: eps
     real(DP), intent(inout) :: N
-    logical, intent(in)  :: basin(NGRID*NGRID)
+    logical, intent(in)     :: basin(NGRID*NGRID)
     logical, dimension(:), allocatable  :: basinP, basinM
     
     allocate(basinP(NGRID*NGRID))
@@ -91,23 +129,32 @@ module henondim
     call henon_map(basinP,eps)  
     call henon_map(basinM,-eps)  
     N = count(basin.ne.basinP.or.basin.ne.basinM)
-  end subroutine henon_compare
-
-  subroutine basin_alg(Neps,N,eps)
+  end subroutine basin_compare
+!-------------------------------------------------------------------------------
+  subroutine basin_alg(Neps,eps,d)
      
 ! BASIN_ALG - implements Algorithm D.
 !
-!  Argument declarations go here.  Explicitly declare all arguments and give
-!  each of them an INTENT.
+! ARGUMENTS - Neps : integer, dimension of 1D arrays N and eps.
+!           - eps  : real, 1D array eps which represents the magnitude of the
+!                    shifting of the initial grid.
+!           - d    : real, d represents the dimension of the basin of attraction. 
 !
-!
-!  Local variables go here.
-!
+! PROCEDURE : We obtain the basin of attraction of the Henon map for the
+!             unshifted grid, basin. Then, for multiple epsilons, we compare
+!             the basins obtained out of shifted grids by a magnitued epsilon.
+!             This is done in the subroutine basin_compare in such a way that
+!             can be parallelizable using OpenMP. Such subroutine returns then
+!             two 1D arrays, the number of epsilon uncertain grid points N,
+!             and the different epsilon used. Those two arrays are passed to
+!             the linfit subroutine to obtain the dimension of the basin of
+!             attraction.
 
-!  The rest of your code goes here.
-!
-    integer  :: j, Neps, ierr
-    real(DP) :: eps(Neps), param(2), N(Neps)
+    integer,  intent(in)  :: Neps
+    real(DP), intent(inout) :: eps(Neps), d
+
+    integer  :: j, ierr
+    real(DP) :: param(2), N(Neps)
     logical, dimension(:), allocatable  :: basin
     
     allocate(basin(NGRID*NGRID))
@@ -115,14 +162,12 @@ module henondim
     call henon_map(basin,0.d0)  
     !$omp parallel do 
     do j=1,Neps
-      print *, "Beginning",j
-      call henon_compare(N(j),eps(j),basin)
-      print *, "End",j
+      call basin_compare(N(j),eps(j),basin)
     enddo
     !$omp end parallel do
 
     call linfit('Power',Neps,eps,N,param,ierr)    
-    if(ierr==0) print *, "d = ",1+param(2)
+    if(ierr==0) d=1+param(2)
     return
   end subroutine basin_alg
 !-------------------------------------------------------------------------------
--- HighPerformanceComputing/Homework5/lsq.f90
+++ HighPerformanceComputing/Homework5/lsq.f90
@@ -6,7 +6,10 @@ module lsq
 !    use lsq
 !
 !  DESCRIPTION
-!    Write a one-sentence description of LINFIT.
+!   The subroutine linfit will calculate the parameters a and b for: 
+!     -- Linear fitting: y=a·x+b.
+!     -- Power fitting: y=a·x^b, using logarithms to linearize.
+!   The code chooses which one to apply based on the given entry "model".
 !
 !  PUBLIC ROUTINES DEFINED
 !    linfit - Y versus X linear regression.
@@ -15,7 +18,7 @@ module lsq
 !    precision - defines KINDs for single- and double-precision floating point.
 !
 !  REVISION HISTORY
-!    10/15/18 - first implementation.
+!    10/20/18 - Final version, including both fittings.
 !
 !  PROGRAMMER
 !    Francisco Castillo-Carrasco, fjcasti1@asu.edu
@@ -49,7 +52,18 @@ module lsq
   contains
 !----------
   subroutine linfit(model, n, x, y, param, ierr)
-!  LINFIT - briefly describe the input and output arguments here.
+      !  ARGUMENTS - model : character variable, expecting "Linear" or "Power", not
+      !                      case sensitive.
+      !            - n     : dimension of the arrays x and y.
+      !            - x     : real 1D array of dimension n, independent variable.
+      !            - y     : real 1D array of dimension n, y=f(x).
+      !            - param : real 1D array of dimention 2, contains the parameters
+      !                      of the fitting.
+      !            - ierr  : error flag, 0 if no errors found.
+      !
+      ! Comments: When using the LAPACK libraries, the code uses it once with
+      ! lwork=-1 to find the optimal lwork and a second time to find the with
+      ! the optimal lwork to solve the system.
 !
   character(*), intent(in) :: model
   integer , intent(in)  :: n
@@ -71,6 +85,7 @@ module lsq
     call dgels('N', n, 2, 1, A, n, y, n, work, lwork, info)
     lwork=work(1)
     deallocate(work)
+
     allocate(work(lwork))
     call dgels('N', n, 2, 1, A, n, y, n, work, lwork, info)
     param(1:2)=y(1:2)
@@ -83,12 +98,15 @@ module lsq
     call dgels('N', n, 2, 1, A, n, B, n, work, lwork, info)
     lwork=work(1)
     deallocate(work)
+
     allocate(work(lwork))
     call dgels('N', n, 2, 1, A, n, B, n, work, lwork, info)
     param(1)=exp(B(1))
     param(2)=B(2)
     ierr=info
   case default
+    ierr=-1
+    print*, "ERROR, MODEL UNKNOWN"
   end select
 !
   end subroutine linfit
