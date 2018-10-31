6466a63be7cb9d56d33e85d7a37ecffb7ea6d8ee
fjcasti1 <fjcasti1@asu.edu>
2018-10-30 21:22:41 -0700

    HPC Hw6, started, stats done

--- HighPerformanceComputing/Homework6/quartile.cc
+++ HighPerformanceComputing/Homework6/quartile.cc
@@ -11,16 +11,60 @@ using namespace std;
 // -----------------------------------------------------------
 //  PROCESS_FILE - read the data, sort it, and compute the quartiles
 //  You may add other input arguments as necessary to adjust the behavior.
+void extremes(vector<double> v,double &min,double &max)
+{
+  min=v[0];
+  max=v[0];
+  for(int i=1;i<v.size();i++)
+  {
+    if(v[i]<min)
+      min=v[i];
+    if(v[i]>max)
+      max=v[i];
+  }
+}
+
+double median(vector<double> v,int begin,int end)
+{
+  int N=end-begin+1;
+  double median;
+  if(N % 2 == 0)
+    median=(v[begin+N/2]+v[begin+N/2-1])/2;
+  else
+    median=v[begin+N/2];
+  return(median);
+}
+
+void quartiles(vector<double> v,double &Q1,double &Q2, double &Q3)
+{
+  int index,N;
+  N=v.size();
+  if(N % 2 == 0)
+  {
+    Q1=median(v,0,N/2-1);
+    Q2=median(v,0,N-1);
+    Q3=median(v,N/2,N-1);
+  }
+  else
+  {
+    Q1=median(v,0,N/2-1);
+    Q2=median(v,0,N-1);
+    Q3=median(v,N/2+1,N-1);
+  }
+}
 
 int process_file(istream& in)
 {
    int retcode = EXIT_SUCCESS;  // unless an error occurs during processing
    vector<double> v;
-   double value;
+   double value,vmin,vmax,Q1,Q2,Q3;
 
-   while(in >> value)
+   while(in >> value){
      v.push_back(value);  // add to the end of the existing vector
-
+   }
+   sort(v.begin(),v.end());
+   extremes(v,vmin,vmax);
+   quartiles(v,Q1,Q2,Q3);
    //  Sort the vector if necessary and compute the requested statistics.
    return(retcode);
 }
