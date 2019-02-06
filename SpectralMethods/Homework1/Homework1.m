0f47c49f7245e08ccfe4e6c1acb1a2cbb7cf0822
Francisco Castillo <fjcasti1@asu.edu>
2019-02-06 14:07:04 -0700

    deleted Camille's hw

--- SpectralMethods/Homework1/Homework1.m
+++ SpectralMethods/Homework1/Homework1.m
@@ -1,130 +1,61 @@
-clear all
-close all
-clc
-
-x = chebfun('x',[0 2*pi]);
-f = 1./(2+sin(x).^2);
-plot(f)
-
- 
-% N=10;
-% A = exp(1i*x*(-N:N));
-% I = A*A^(-1)
-% I(1,1)
-% cccc
-N = 20:10:150;
-for i=1:length(N)
-    A = exp(1i*x*(-N(i):N(i)));
-    fhat_k = (1/2*pi)*A'*f;
-    fN = A*fhat_k;
-    err(i) = max(abs(f-fN));
-    i
-end
-semilogy(N,err,'*')
-%%
-
-close all; clear variables
-
+clear variables; close all; clc
+figformat='epsc';
 %% Problem 4
-close all
-N = 20;
+N = 20:10:500;
 x = chebfun('x',[0 2*pi]);
-f = sin(x./2).^3;
-figure
-plot(f)
-
-A = exp(1i*x*(-N:N));
-lambda = (1/2*pi)*A'*f;
-
-% figure
-% plot(real(lambda),imag(lambda),'*')
+f = sin(x/2).^3;
 
 figure
-loglog(-N:N,lambda,'*')
+plot(f,'linewidth',2)
+grid on
+xlabel('$x$','interpreter','latex')
+ylabel('$f(x)=\sin^3(x/2)$','interpreter','latex')
+set(gca,'fontsize',14)
+txt='Latex/FIGURES/P4_1';
+saveas(gcf,txt,figformat)
 
-%%
-clear variables;
-N = 20:10:100;
-x = chebfun('x',[0 2*pi]);
-f = sin(x./2).^3;
 for j = 1:length(N)
     A = exp(1i*x*(-N(j):N(j)));
-    lambda = (1/2*pi)*A'*f;
+    lambda = 1/(2*pi)*A'*f;
     fn = A*lambda;
-    err(j) = norm(f-fn,2);
+    err(j) = norm(f-fn,Inf);
 end
 
 figure
-semilogy(err,'*')
+loglog(err,'*','MarkerSize',12)
+grid on
+xlabel('$N$','interpreter','latex')
+ylabel('Error','interpreter','latex')
+set(gca,'fontsize',14)
+txt='Latex/FIGURES/P4_2';
+saveas(gcf,txt,figformat)
 
 %% Problem 5
-
-N = 500;
-
+N = 20:10:500;
 x = chebfun('x',[0 2*pi]);
 f = 3./(5-4*cos(x));
-figure
-plot(f)
-
-A = exp(1i*x*(-N:N));
-lambda = (1/2*pi)*A'*f;
-
-a = acos(5/4)
-fa = 3./(5-4*cos(x-a*1i));
-M = max(fa);
-errbound = 4*pi*M/(a*(exp(-a*N)))
-lambdabound = 2*pi*M*exp(-a*N)
 
 figure
-loglog(-N:N,lambda,'*')
-hold on
-loglog(-N:N,errbound*ones(length(-N:N)),'r')
-loglog(-N:N,lambdabound*ones(length(-N:N)),'g')
+plot(f,'linewidth',2)
+grid on
+xlabel('$x$','interpreter','latex')
+ylabel('$f(x)=\frac{3}{5-4\cos(x)}$','interpreter','latex')
+set(gca,'fontsize',14)
+txt='Latex/FIGURES/P5_1';
+saveas(gcf,txt,figformat)
 
-%% 
-N = 20:10:100;
-for i = 1:length(N)
-x = chebfun('x',[0 2*pi]);
-f = 1./(2+sin(x).^2);
-A = exp(1i*x*(-N(i):N(i)));
-lambda = (1/(2*pi)*A'*f);
-fn = A*lambda;
-
-err(i) = norm(f - fn,2);
+for j = 1:length(N)
+    A = exp(1i*x*(-N(j):N(j)));
+    lambda = 1/(2*pi)*A'*f;
+    fn = A*lambda;
+    err(j) = norm(f-fn,Inf);
 end
-%%
-figure
-loglog(N,err,'*')
-
-% Z = asin(sqrt(2)*1i);
-
-% figure
-% semilogy(-N:N,lambda,'*')
-
-%%
-close all
-clear all
-clc
-
-x = chebfun('x',[0 2*pi]);
-f = 1./(2+sin(x).^2);
-A = exp(1i*x*(-10:10));
-lambda = (1/(2*pi)*A'*f);
-
-fn = A*lambda;
-err = norm(f-fn,2)
-
-
-zroot = asin(sqrt(2)*1i);
-a = imag(zroot)
-fa = 1./(2+sin(x-a).^2);
-M = max(fa)
-errbound = 4*pi*M/a*(exp(-a*abs((-10:10))))';
-lambdabound = 2*pi*M*exp(-a*abs((-10:10)))';
 
 figure
-semilogy(-10:10,lambda,'*')
-hold on
-semilogy(-10:10,errbound,'r')
-semilogy(-10:10,lambdabound,'g')
-
+loglog(err,'*','MarkerSize',12)
+grid on
+xlabel('$N$','interpreter','latex')
+ylabel('Error','interpreter','latex')
+set(gca,'fontsize',14)
+txt='Latex/FIGURES/P5_2';
+saveas(gcf,txt,figformat)
